ead52ad86476f36f3a9e70d4d45425e4de29e461
Arpit <arpit@arpitsharma.in>
2024-08-11 13:40:02 +0530

    Trees file added

---
+++ binary_tree/index.js
@@ -0,0 +1,51 @@
+class BstNode {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insertByKey(key) {
+    const newNode = new BstNode(key);
+
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    // node -> root node
+    if (newNode.key < node.key) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  delete(key) {
+    this.root = this.deleteNode(this.root, key);
+  }
+
+  deleteNode(node, key) {
+    // node -> root
+    if (node === null) {
+      return null;
+    }
+  }
+}
